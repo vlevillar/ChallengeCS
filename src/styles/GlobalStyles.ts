9f6331fe56201b5c9776037dbd8bf3ceeeb5e937
Valentino Villar <vles2345@gmail.com>
2024-11-07 03:49:04 -0300

    fonts fix

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -3,21 +3,21 @@ import { createGlobalStyle } from 'styled-components';
 const GlobalStyles = createGlobalStyle`
   @font-face {
     font-family: 'SF Pro';
-    src: url('/public/fonts/SF-Pro-Regular.otf') format('opentype');
+    src: url('/src/assets/fonts/SF-Pro-Regular.otf');
     font-weight: 400;
     font-style: normal;
   }
 
   @font-face {
     font-family: 'SF Pro';
-    src: url('/public/fonts/SF-Pro-Medium.otf') format('opentype');
+    src: url('/src/assets/fonts/SF-Pro-Medium.otf');
     font-weight: 500;
     font-style: normal;
   }
 
   @font-face {
     font-family: 'SF Pro';
-    src: url('/public/fonts/SF-Pro-Bold.otf') format('opentype');
+    src: url('/src/assets/fonts/SF-Pro-Bold.otf');
     font-weight: 700;
     font-style: normal;
   }
@@ -36,7 +36,7 @@ const GlobalStyles = createGlobalStyle`
 html, body {
   margin: 0;
   padding: 0;
-  overflow-x: hidden; /* Para evitar cualquier desplazamiento horizontal inesperado */
+  overflow-x: hidden;
   width: 100%;
   height: 100%;
 }
