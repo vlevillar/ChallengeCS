4c6f12d77d9d40df26cd93ec20f761712792be0b
Valentino Villar <vles2345@gmail.com>
2024-11-07 03:50:57 -0300

    fonts fix

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -3,21 +3,21 @@ import { createGlobalStyle } from 'styled-components';
 const GlobalStyles = createGlobalStyle`
   @font-face {
     font-family: 'SF Pro';
-    src: url('/src/assets/fonts/SF-Pro-Regular.otf');
+    src: url('/fonts/SF-Pro-Regular.otf');
     font-weight: 400;
     font-style: normal;
   }
 
   @font-face {
     font-family: 'SF Pro';
-    src: url('/src/assets/fonts/SF-Pro-Medium.otf');
+    src: url('/fonts/SF-Pro-Medium.otf');
     font-weight: 500;
     font-style: normal;
   }
 
   @font-face {
     font-family: 'SF Pro';
-    src: url('/src/assets/fonts/SF-Pro-Bold.otf');
+    src: url('/fonts/SF-Pro-Bold.otf');
     font-weight: 700;
     font-style: normal;
   }
